651051dafe05e2fd840313948de27bed998fb78e
Kumar Vijay <vijay81238@gmail.com>
2023-02-25 19:07:08 +0530

    Initial commit

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,22 @@
+var fullName = "Kumar Vijay";
+
+
+//how to name varibales
+
+//fullName    - It is camel case (OK)
+//full_name    - Ok
+//full_name   -it is snake case  Ok
+//FullName      - Not Ok
+
+var courseName = "React JS bootcamp";
+
+var isLoggedIn = false;
+
+
+var loggedCount = 34;
+console.log(loggedCount);
+
+var paymentMode;
+console.log(paymentMode);
+
+var paymentMode = "Credit card";
