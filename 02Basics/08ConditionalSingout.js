0ba5d93e2b6a69a55b65af9c36801d798c6f4487
Kumar Vijay <vijay81238@gmail.com>
2023-02-26 15:32:35 +0530

    Ternary operator in javascript

---
+++ 02Basics/08ConditionalSingout.js
@@ -0,0 +1,14 @@
+//Show user a signout button if he is authenicated
+//otherwise show him option to Login/Signup
+
+
+var authenticated = false;
+
+// if(authenticated){
+//     console.log("Show singout button");
+// }
+// else{
+//     console.log("Show login option");
+// }
+
+authenticated  ? console.log("Show signout button") : console.log("Show login option");
