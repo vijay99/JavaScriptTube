f4ffebd68677f3243e44ad973d8272daac286439
Kumar Vijay <vijay81238@gmail.com>
2023-02-26 17:22:24 +0530

    Functions in JavaScript

---
+++ 03Intermediate/01Functions.js
@@ -0,0 +1,15 @@
+function sayHello(name){
+   // console.log("hello from function");
+    console.log(`Hello there , ${name}. How are you ?`);
+}
+
+// sayHello("vijay"); //calling of function
+// sayHello("Sammy");  //refrencing a function
+
+function namstey() {
+    return "Hello in India";
+}
+
+var greeting = namstey();
+//console.log(greeting);
+console.log(namstey())
