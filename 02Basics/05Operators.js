924b8fab42a5f6c1a2f535007db6bbf059653fa5
Kumar Vijay <vijay81238@gmail.com>
2023-02-26 15:20:06 +0530

    Some ore basics of java script - operators and consitional

---
+++ 02Basics/05Operators.js
@@ -0,0 +1,9 @@
+var sellingPrice = 199;
+
+var listingPrice = 799;
+
+var result = listingPrice > sellingPrice;
+
+console.log(result);
+console.log(typeof result);
+console.log(typeof listingPrice);
---
+++ 02Basics/06Conditionals.js
@@ -0,0 +1,18 @@
+var temperature;
+
+//to do : GO to google and get the data
+
+temperature = 39;
+
+if(temperature < 20){
+    console.log("It is very cold outside");
+}
+
+if(temperature < 30){
+    console.log("It is moderate temperature outside");
+}
+else{
+    console.log("it is really hot outside.")
+}
+
+
