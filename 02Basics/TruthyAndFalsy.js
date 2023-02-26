4161bd5a800505d505886dc0a87c999b562bd90b
Kumar Vijay <vijay81238@gmail.com>
2023-02-26 16:26:14 +0530

    Truthy and Falsy

---
+++ 02Basics/TruthyAndFalsy.js
@@ -0,0 +1,23 @@
+//TODO: falsy  
+// undefined
+// null
+// 0
+// ''
+// NaN
+
+
+var user ="2"; //undefined
+
+if(user==2){
+    console.log("Condition is true");
+}
+
+//for strict checking
+if(user===2){
+    console.log("Condition is true");
+}
+
+
+// console.log(2+"2");
+// console.log("2"+2);
+
