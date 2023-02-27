84b7517732d286c18603ac00273e0e0aa1bcbe6f
Kumar Vijay <vijay81238@gmail.com>
2023-02-27 20:26:56 +0530

    Array slice and splice

---
+++ 03Intermediate/10ArraySlice.js
@@ -0,0 +1,9 @@
+var users = ["Ted","Tim","Ton","Sam","Sor","Sod"];
+
+// console.log(users.slice(1,3));
+
+//console.log(users.slice(1));
+
+users.splice(1,2,"HI");
+console.log(users);
+users.splice
