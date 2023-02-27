d0d797a98424fa0261493a3969a02a5d65021700
Kumar Vijay <vijay81238@gmail.com>
2023-02-27 21:43:11 +0530

    loop

--- 03Intermediate/13Loop.js
+++ 03Intermediate/13Loop.js
@@ -1,10 +1,33 @@
-for(let i = 0;i<10;i++){
-    console.log(i);
-}
+// for(let i = 0;i<10;i++){
+//     console.log(i);
+// }
+
+// const myStates = ["Rajasthan","Delhi","Assam",1947,"Tamil Nadu","Maharastra"];
+
+// for(let i = 0;i<myStates.length;i++){
+//     if( typeof myStates[i]!=='string') continue;
+//     console.log(myStates[i]);
+// }
+
+// const myStates = ["Rajasthan","Delhi","Assam",1947,"Tamil Nadu","Maharastra"];
+
+// let i = 0;
+
+// while(i < myStates.length){
+//     console.log(myStates[i]);
+//     i++;
+
+// }
+
 
 const myStates = ["Rajasthan","Delhi","Assam",1947,"Tamil Nadu","Maharastra"];
 
-for(let i = 0;i<myStates.length;i++){
-    if( typeof myStates[i]!=='string') continue;
+let i = 0;
+
+do{
     console.log(myStates[i]);
-}
+    i++;
+
+} while(i < myStates.length);
+
+
