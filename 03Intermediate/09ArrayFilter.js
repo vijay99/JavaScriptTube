d3af5117093327c9a0eabab9174acc91fa8b5251
Kumar Vijay <vijay81238@gmail.com>
2023-02-27 20:16:04 +0530

    Array fill and filter

---
+++ 03Intermediate/09ArrayFilter.js
@@ -0,0 +1,10 @@
+var testArray = [2,4,6,3,1,5,9,8];
+
+//console.log(testArray.fill('a',4));
+
+console.log(testArray.fill('a',4,5));
+
+const myNumber = [23,24,25,55,66,77,87,98];;
+
+const result = myNumber.filter((num) => num!=55);
+console.log(result);
