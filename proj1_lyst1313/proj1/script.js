3e6b64e65cb16fa02b673e8ed6b1014603db8930
Kumar Vijay <vijay81238@gmail.com>
2023-02-28 16:16:50 +0530

    Mini project 1 JavaScript

---
+++ proj1_lyst1313/proj1/script.js
@@ -0,0 +1,18 @@
+var counter = document.querySelector(".counter");
+var followers = document.querySelector(".followers");
+
+counter.innerHTML = 100;
+
+let count = 1;
+
+setInterval( () => {
+    if(count < 1000){
+    count++;
+    counter.innerText= count;
+    }
+},1);
+
+
+setTimeout( () => {
+    followers.innerText = "Followers on Instagram.";
+},5000)
