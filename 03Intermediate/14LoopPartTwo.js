58e5f6e0e37b1ea18f4536cc4713ce0a7f1d6c0f
Kumar Vijay <vijay81238@gmail.com>
2023-02-28 06:26:25 +0530

    forEach() loop

---
+++ 03Intermediate/14LoopPartTwo.js
@@ -0,0 +1,7 @@
+const myStates = ["Rajasthan","Delhi","Assam",1947,"Tamil Nadu","Maharastra"];
+
+//forEach() loop
+//myStates.forEach( (s) => (console.log(s)));
+
+//remove extra parentesis 
+myStates.forEach( s => console.log(s));
