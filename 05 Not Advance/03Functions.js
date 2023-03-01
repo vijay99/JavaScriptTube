328c7cad328a1ad2cbfce2ecbc098bcffdbc1745
Kumar Vijay <vijay81238@gmail.com>
2023-03-01 07:42:34 +0530

    Self execution anonymous function

---
+++ 05 Not Advance/03Functions.js
@@ -0,0 +1,10 @@
+function sayHello() {
+    console.log("Hello from Vijay");
+};
+
+sayHello();
+
+//self execution anonymous function
+(function(){
+    console.log("This is self execution anonymous function");
+})();
