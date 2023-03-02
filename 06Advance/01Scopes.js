f7c933a5499d3a5685387c6749c1ee02541faf52
Kumar Vijay <vijay81238@gmail.com>
2023-03-02 05:39:44 +0530

    Scope

---
+++ 06Advance/01Scopes.js
@@ -0,0 +1,23 @@
+//console.log(name);
+
+//var name = "vijay";
+
+//replacing var with let and it actually gives error 
+//which is required in backend code 
+
+//let name = "vijay";
+
+
+
+
+var name = "vijay";
+
+if(true){
+    //var lastName = "Choudhary";
+    let lastName = "Choudhary";
+   // console.log(lastName);
+}
+
+//I am keeping out of if but still it is printing ,it is an issue
+//So the solution is ,we should use let 
+console.log(lastName);
