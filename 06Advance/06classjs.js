f4bd7a3faca97d7d3ed75af51faf07c55b4a3979
Kumar Vijay <vijay81238@gmail.com>
2023-03-02 08:32:27 +0530

    Private pops getters and setters

--- 06Advance/06classjs.js
+++ 06Advance/06classjs.js
@@ -3,18 +3,18 @@ class User {
         this.name=name;
         this.email=email;
     }
-    courseList = [];
+    #courseList = [];
 
     getInfo() {
         return {name : this.name,email: this.email};
     }
 
     enrollCourse(name){
-        this.courseList.push(name);
+        this.#courseList.push(name);
     }
 
     getCourseList(){
-        return this.courseList;
+        return this.#courseList;
     }
 
 }
@@ -22,3 +22,14 @@ class User {
 //let vijay = new User("vijay","vijay8123@gmail.com");
 
 module.exports = User;  //now any other file can access content from outside
+
+
+
+
+
+
+const rock = new User("rock","rock@rock.com");
+console.log(rock.getInfo());
+rock.enrollCourse("Java");
+console.log(rock.getCourseList());
+console.log(rock.courseList);
