ace888dd77b7bea1105c560c42c64670e858eeb6
Kumar Vijay <vijay81238@gmail.com>
2023-03-03 18:19:24 +0530

    promise async resolve reject

---
+++ 06Advance/10promises.js
@@ -0,0 +1,35 @@
+const uno =()  => {
+    return "I am one";
+};
+
+// const dos = async ()  => { 
+//     setTimeout( () => {
+//         return "I am two";
+//     },3000)
+// };
+ const dos = () => {
+    return new Promise((resolve,reject) => {
+        setTimeout( () => {
+            //reject("I am two");
+            resolve("I am Two");
+        },3000);
+    }
+    );
+ };
+
+const tres =()  => {
+   return "I am Three";
+};
+
+const callMe = async () => {
+    let valOne = uno();
+    console.log(valOne);
+
+    let valTwo = await dos();
+    console.log(valTwo);
+
+    let valThree = tres();
+    console.log(valThree);
+}
+
+callMe();
